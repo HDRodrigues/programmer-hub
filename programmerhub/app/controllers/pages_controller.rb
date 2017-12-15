3b5e28a58f39e36dae839f4fb7e37bdcb455a901
Helton <helton.rodrigues@gt4w.com.br>
2017-12-15 20:43:43 -0200

    Adicao de configuracao para o devise pular a pagina post por não precisar ser validado o usuario

--- programmerhub/app/controllers/pages_controller.rb
+++ programmerhub/app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
+  # Essa pagina nao necessita de devise por ser uma página de aprsentação da aplicação
+  skip_before_filter :authenticate_user!
+  
   def home
   end
 end
