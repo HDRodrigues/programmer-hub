a54de8cf6633f60d4ced7a82f97635787f55f859
Helton <helton.rodrigues@gt4w.com.br>
2017-12-16 08:56:17 -0200

    override nos metodos do devise para jogar ao user para a pagina dele de post ou para a principal e tratamento de record_not_found

--- programmerhub/app/controllers/application_controller.rb
+++ programmerhub/app/controllers/application_controller.rb
@@ -1,15 +1,25 @@
 class ApplicationController < ActionController::Base
-  # Verifica se está autenticado e redireciona caso contrario
-  before_action :authenticate_user!
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  
+  #Captura de evento no rails
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
 
   protected
   
-    #def after_sign_in_path_for(current_user)
-    #  posts_path
-    #end
+    def after_sign_in_path_for(current_user)
+      posts_path
+    end
   
-    #def after_sign_up_path_for(current_user)
-    #  posts_path
-    #end
+    def after_sign_up_path_for(current_user)
+      posts_path
+    end
+
+  private
+    
+    def record_not_found
+      flash[:alert] = "Record not Found"
+      redirect_to root_url
+    end
 end
