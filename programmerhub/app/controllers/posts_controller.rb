2da2fe7526c679bc76f89d67f2b1a0a9bae7cfb0
Helton <helton.rodrigues@gt4w.com.br>
2017-12-15 23:06:24 -0200

    controller fixed

--- programmerhub/app/controllers/posts_controller.rb
+++ programmerhub/app/controllers/posts_controller.rb
@@ -5,28 +5,23 @@ class PostsController < ApplicationController
 	def index
 		@user = current_user 
 		@post = Post.new
-		# array de posts sem associacao por isso o map
-    posts = current_user.posts.map {|post| post}
-    #pega todos os seguidores 
-      #para todos os posts do seguidor
-        #pega os posts
-    current_user.all_following.each do |user| 
-      user.posts.each do |post| 
-				posts << post
+		posts = current_user.posts.map {|post| post}
+		current_user.all_following.each { |user| user.posts.each{|post| posts << post } }
 		@posts = posts.sort_by &:created_at
 	end
 
 	def create
 		@post = Post.new(post_params)
+
 		respond_to do |format|
-			if @post.save
-				format.json { render json: @post, status: :created }
-				format.html { redirect_to posts_path, notice: 'Post was successfully created.' }
-			else
-				format.json { render json: @post.errors, status: :unprocessable_entity }
-				format.html { redirect_to posts_path }
-			end
+		if @post.save
+			format.json { render json: @post, status: :created }
+			format.html { redirect_to posts_path, notice: 'Post was successfully created.' }
+		else
+			format.json { render json: @post.errors, status: :unprocessable_entity }
+			format.html { redirect_to posts_path }
 		end
+			end
 	end
 
 	def show
@@ -38,9 +33,9 @@ class PostsController < ApplicationController
 		@post.destroy
 
 		respond_to do |format|
-			format.json { head :no_content }
-			format.html { redirect_to posts_path, notice: 'Post was successfully destroyed.' }
-		end
+		format.json { head :no_content }
+		format.html { redirect_to posts_path, notice: 'Post was successfully destroyed.' }
+			end
 	end
 
 	def edit
@@ -51,11 +46,11 @@ class PostsController < ApplicationController
 
 	private
 
-		def set_post
-			@post = Post.find(params[:id])
+	def set_post
+	@post = Post.find(params[:id])
 		end
 
 		def post_params
-			params.require(:post).permit(:body).merge(user: current_user)
+	params.require(:post).permit(:body).merge(user: current_user)
 		end
-end
+	end
