530bd70e288a852f2a2a15f09dc061095e369d7a
Helton <helton.rodrigues@gt4w.com.br>
2017-12-16 08:55:05 -0200

    Configuracao para apagar os likes dos post quando um post é apagado

--- programmerhub/app/models/post.rb
+++ programmerhub/app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :likes
+  #destroi o like quando um post é excluído
+  has_many :likes, dependent: :destroy
   acts_as_commentable
 end
